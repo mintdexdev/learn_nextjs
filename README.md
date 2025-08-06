603ac18b90c62fab2de462d61f13428005c7b966
mintdexdev <dexdevpersona@gmail.com>
2025-08-07 01:33:52 +0530

    new setup

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ To get you the local copy of the repo:
 git clone https://github.com/mintdexdev/learn_nextjs.git
 cd learn_nextjs
 ```
+
+To run the develpment server:
+```
+npm i
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-center">
+        Mintdexdev
+      </h1>
+    </div>
+  )
+}
+
+export default page
